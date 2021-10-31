9fcbfdf3f501119aabe6b2565befc3ab59194c59
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-31 05:40:22 -0700

    created a modal to edit the entries

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { Fragment, useState} from 'react';
 import { Grid, Icon, Segment } from 'semantic-ui-react';
+import ModalEdit from './ModalEdit';
 
 function EntryLine({ id, description, value, isExpense = false, deleteEntry }) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <Segment color={isExpense ? 'red' : 'green'}>
-            <Grid columns={3} textAlign='right'>
-                <Grid.Row>
-                <Grid.Column width={10} textAlign='left'>
-                    {description}
-                </Grid.Column>
-                <Grid.Column width={3} textAlign='right'>
-                    {value}
-                </Grid.Column>
-                <Grid.Column width={3}>
-                    <Icon name='edit' bordered />
-                    <Icon name='trash' bordered onClick={() => deleteEntry(id)}/>
-                </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
+        <Fragment>
+            <Segment color={isExpense ? 'red' : 'green'}>
+                <Grid columns={3} textAlign='right'>
+                    <Grid.Row>
+                    <Grid.Column width={10} textAlign='left'>
+                        {description}
+                    </Grid.Column>
+                    <Grid.Column width={3} textAlign='right'>
+                        {value}
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Icon name='edit' bordered onClick={() => setIsOpen(!isOpen)}/>
+                        <Icon name='trash' bordered onClick={() => deleteEntry(id)}/>
+                    </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+            <ModalEdit isOpen={isOpen} />
+        </Fragment>
     );
 }
 
