ddc9d5e073e52dc5bfcaadf5eebd1a61214f7ff9
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-31 07:16:38 -0700

    fixed modal close button

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -22,7 +22,7 @@ function EntryLine({ id, description, value, isExpense = false, deleteEntry }) {
                     </Grid.Row>
                 </Grid>
             </Segment>
-            <ModalEdit isOpen={isOpen} />
+            <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen} />
         </Fragment>
     );
 }
