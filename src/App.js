f1be9e66bc4b4e16060c2b3484faeb7c6a7f742b
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-30 21:35:56 -0700

    display balances coponent created

--- src/App.js
+++ src/App.js
@@ -13,28 +13,15 @@ import ButtonSaveOrCancel from './components/ButtonSaveOrCancel';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
 
 function App() {
   return (
     <Container>
       <MainHeader title='Budget' />
-      <Statistic size='small'>
-        <Statistic.Label>Your balance:</Statistic.Label>
-        <Statistic.Value>2,555.567</Statistic.Value>
-      </Statistic>
-      <Segment textAlign='center'>
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column>
-              <DisplayBalance title='Income' value='1253.54' color='green' />
-            </Grid.Column>
-            <Grid.Column>
-              <DisplayBalance title='Expenses' value='623.50' color='red' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+      <DisplayBalance title='Your Balance:' value='2,550.53' size='small' />
 
-      </Segment>
+      <DisplayBalances />
 
       <MainHeader title='History' type='h3' />
       <Segment textAlign='center' color="red">
