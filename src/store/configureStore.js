f1a0b362b691954f2469725ef6106757640a1783
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-13 00:30:31 -0800

    installing and configuring redux dev tools completed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,15 @@
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import entriesReducer from '../reducers/entries.reducers';
 
 const configureStore = () => {
     return createStore(
         combineReducers({
             entries: entriesReducer,
-        })
+        }),
+        composeWithDevTools()
+
     );
 };
 
