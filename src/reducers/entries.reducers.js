a9928f676e0d1f01abc82abe6ffc81b97043c384
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-18 23:33:14 -0800

    moddlewares added

--- src/reducers/entries.reducers.js
+++ src/reducers/entries.reducers.js
@@ -1,54 +1,28 @@
-// eslint-disable-next-line import/no-anonymous-default-export
+import entriesTypes from '../actions/entries.actions';
+
 const reducer = (state = initialEntries, action) => {
-    console.log(action);
     let newEntries;
     switch (action.type) {
-        case 'ADD_ENTRY': 
-            newEntries = state.concat({
-            id: 5,
-            description: "hello from redux",
-            isExpense: false
-            })
+        case entriesTypes.POPULATE_ENTRIES:
+            return action.payload;
+        case entriesTypes.ADD_ENTRY_RESULT:
+        newEntries = state.concat({ ...action.payload });
             return newEntries;
-        case 'REMOVE_ENTRY': 
-        newEntries = state.filter((entrv) => entrv.id !== action.payload.id);
-        return newEntries;
-        case 'UPDATE_ENTRY':
-            newEntries = [...state];
-            const index = newEntries.findIndex(
-                (entry) => entry.id === action.payload.id
-            );
-            newEntries[index] = { ...newEntries[index], ...action.payload.entry };
-        return newEntries;
+        case entriesTypes.REMOVE_ENTRY_RESULT:
+        newEntries = state.filter((entry) => entry.id !== action.payload.id);
+            return newEntries;
+        case entriesTypes.POPULATE_ENTRY_DETAILS:
+        case entriesTypes.UPDATE_ENTRY:
+        newEntries = [...state];
+        const index = newEntries.findIndex(
+            (entry) => entry.id === action.payload.id
+        );
+        newEntries[index] = { ...newEntries[index], ...action.payload.entry };
+            return newEntries;
         default:
-        return state;
+            return state;
     }
 };
 export default reducer;
 
-var initialEntries = [
-    {
-        id: 1,
-        description: 'Work income',
-        value: 1000.0,
-        isExpense: false,
-    },
-    {
-        id: 2,
-        description: 'Water bill',
-        value: 20.0,
-        isExpense: true,
-    },
-    {
-        id: 3,
-        description: 'Rent',
-        value: 300,
-        isExpense: true,
-    },
-    {
-        id: 4,
-        description: 'Power bill',
-        value: 50,
-        isExpense: true,
-    },
-];
+var initialEntries = [];
